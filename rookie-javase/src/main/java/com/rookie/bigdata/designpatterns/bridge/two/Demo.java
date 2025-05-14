cebc5c2dbb2c72a5386a6d4957c36bb8a3353047
liuxili01 <liuxili@bestseller.com.cn>
2025-05-14 14:03:27 +0800

    桥接模式

---
+++ rookie-javase/src/main/java/com/rookie/bigdata/designpatterns/bridge/two/Demo.java
@@ -0,0 +1,28 @@
+package com.rookie.bigdata.designpatterns.bridge.two;
+
+/**
+ * @Class Demo
+ * @Description
+ * @Author rookie
+ * @Date 2025/5/14 14:02
+ * @Version 1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
