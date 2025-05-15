e4070080a56093a3dd5b417f6b4036ab4138f609
liuxili01 <liuxili@bestseller.com.cn>
2025-05-15 16:45:05 +0800

    外观模式

---
+++ rookie-javase/src/main/java/com/rookie/bigdata/designpatterns/face/runoob/Circle.java
@@ -0,0 +1,16 @@
+package com.rookie.bigdata.designpatterns.face.runoob;
+
+/**
+ * @Class Circle
+ * @Description
+ * @Author rookie
+ * @Date 2025/5/15 16:43
+ * @Version 1.0
+ */
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Circle::draw()");
+    }
+}
